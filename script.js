13de2baa9f34465d31c5fd6d2e823f9520bb40a8
Saiisshh <118446344+saishpawar@users.noreply.github.com>
2023-03-17 16:59:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,56 @@
 // Your Script here.
+
+var map = {
+  A: "N",B: "O",C: "P",D: "Q",E: "R",F: "S",G: "T",H: "U",
+  I: "V",
+  J: "W",
+  K: "X",
+  L: "Y",
+  M: "Z",N: "A",
+  O: "B",
+  P: "C",
+  Q: "D",
+  R: "E",
+  S: "F",
+  T: "G",
+  U: "H",
+  V: "I",
+  W: "J",
+  X: "K",
+  Y: "L",
+  Z: "M",
+  
+};
+
+var string=(aaa);
+
+string=string.split(" ");
+var ans=[];
+for(var i=0;i<string.length;i++)
+	{
+		var wordDec=string[i];
+		var wordEnc
+		for(let s=0;s<wordDec.length;s++)
+			{
+				wordEnc+=map[wordDec.charAt(s)];
+			}
+
+		ans.push(wordEnc);
+	}
+
+return ans.join(" ");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
